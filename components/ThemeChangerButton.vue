57bad1d0227ff8206c8f8be2cf5d173b56f6d34d
mhossain <m.hossain@blackcover.fr>
2021-03-26 23:54:01 +0100

    reposition theme toggler 2

--- components/ThemeChangerButton.vue
+++ components/ThemeChangerButton.vue
@@ -2,7 +2,7 @@
   <button class="ml-5 w-8 h-8 rounded-full focus:outline-none" @click="toggle">
     <span
       :class="[
-        this.$colorMode.value === 'dark' ? 'text-gray-200' : 'text-gray-600',
+        this.$colorMode.value === 'dark' ? 'text-yellow-500' : 'text-gray-600',
       ]"
       class="duration-300 material-icons leading-8"
     >
